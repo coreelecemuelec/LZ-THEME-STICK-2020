2b57859e59a24152e7e3e86fd594375349e6c656
RetroStudios <coreelecemuelec@gmail.com>
2023-05-21 13:11:56 -0300

    temanovo

--- assets/backgrounds/default/convert-symlink.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-counter=0
-until [ $counter -gt 250 ]
-do
-echo -n "Filename : "
-read filename
-cp --remove-destination `readlink ${filename}.jpg` ${filename}.jpg
- ((counter++))
-done
